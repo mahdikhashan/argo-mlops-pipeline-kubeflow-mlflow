214519bf6dcc14d3c76104ce47910a696ce300bf
mahdikhashan <mahdikhashan1@gmail.com>
2024-11-24 17:39:12 +0100

    remove

--- learning-samples/connect-to-pipeline.py
+++
@@ -1,9 +0,0 @@
-from kfp.client import Client
-
-client = Client(host='http://localhost:3000')
-run = client.create_run_from_pipeline_package(
-    'pipeline.yaml',
-    arguments={
-        'recipient': 'World',
-    },
-)
